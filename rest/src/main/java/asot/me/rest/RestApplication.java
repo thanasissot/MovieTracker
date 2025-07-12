28dbce2597d309187179d763d6caa6a74d85ca7b
thanasissot <thanasissot1@gmail.com>
2025-07-12 22:15:06 +0300

    initial commit

---
+++ rest/src/main/java/asot/me/rest/RestApplication.java
@@ -0,0 +1,13 @@
+package asot.me.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+
+}
